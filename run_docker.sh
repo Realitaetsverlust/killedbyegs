13bbcb8ee9d979a07653a178b7157b32da62ca38
Realitaetsverlust <realitaetsverlust@realitaetsverlust.rocks>
2023-04-06 16:24:51 +0200

    Fixed run_docker.sh so it works on other systems aswell

--- run_docker.sh
+++ run_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -p 80:80 -p 3306:3306 -p 2222:22 -v /home/rv/Projects/private/killedbyegs:/var/www/html --name killedbyegs -d lamp
+docker run -p 80:80 -v $(pwd):/var/www/html --name killedbyegs -d lamp
